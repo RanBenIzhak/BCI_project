c714c76608ff92a37c3a3832c8ab5c17400f904c
Ran Ben Izhak <benizhakran@gmail.com>
2017-09-09 11:09:00 +0300

    update

--- eeg_analysis.py
+++ eeg_analysis.py
@@ -48,6 +48,29 @@ def butter_bandpass_filter(data, lowcut=LOW_CUT, highcut=HIGH_CUT, fs=FS, order=
     y = signal.lfilter(b, a, data, axis=0)
     return y
 
+def avg_timeframe(eeg_list):
+    '''   
+    :param eeg_list: list of eeg recordings, each in shape [samples, channels]
+    :return: average signal in frequency domain for the given eeg list
+    '''
+    assert eeg_list[0].shape == (256, len(ACTIVE_CHANNELS))
+    eeg_sum = np.zeros_like(eeg_list[0])
+    for i in range(len(eeg_list)):
+        eeg_sum += abs(fftpack.fft(eeg_list[i], axis=0))
+    return eeg_sum / len(eeg_list)
+
+def sub_mean(eeg_frame):
+    '''
+    substract the mean of each channel (each column) from each channel
+    :param eeg_frame: 2d array of [samples, channels]
+    :return: [ch1 - mean(ch1), ch2 - mean(ch2), ... ]
+    '''
+    rows = eeg_frame.shape[0]
+    eeg_frame_mean = np.matlib.repmat(np.mean(eeg_frame, axis=0), rows, 1)
+    return eeg_frame - eeg_frame_mean
+# -------- Functions end --------------
+
+
 # --- Part A - loading saved data (working offline) --- #
 # ----------------------------------------------------- #
 records_path = 'C:\\Users\\ranbenizhak\\OneDrive\\BCI\\BCI_project\\Data\\'
@@ -62,18 +85,26 @@ new_shape = (int(eeg_data.shape[0] / FS) , FS, eeg_data.shape[1])
 eeg_data_per_sec = np.reshape(eeg_data[0: new_shape[0]*FS, :], new_shape)
 assert np.array_equal(eeg_data_per_sec[0, :, :] , eeg_data[0:FS, :]) # sanity check
 t, r, c = eeg_data_per_sec.shape
-eeg_data_centered = [eeg_data_per_sec[x, ...] - np.matlib.repmat(np.mean(eeg_data_per_sec[x, ...], axis=0),r, 1) for x in range(t)]
-# --- Part B - filtering the data --- #
+eeg_data_centered = [sub_mean(eeg_data_per_sec[x, ...]) for x in range(t)]
 
+# --- Part B - filtering the data ------------------- #
+# --------------------------------------------------- #
 # BPF - passing  2.5 - 45 [Hz]
 eeg_filtered = [butter_bandpass_filter(eeg_data_centered[x]) for x in range(t)]
-eeg_example_unfilt = fftpack.fft(eeg_data_per_sec[10, :, 0])
-eeg_example_filt = fftpack.fft(eeg_filtered[10][:, 0])
 
+# # Visualizing results
+# eeg_example_unfilt = fftpack.fft(eeg_data_per_sec[10, :, 0])
+# eeg_example_filt = fftpack.fft(eeg_filtered[10][:, 0])
 x_tick = np.arange(0., FS/2, 0.5)
-fig = plt.figure(2)
-plt.plot(x_tick, 20*np.log10(np.abs(eeg_example_unfilt)), 'r--', x_tick, 20*np.log10(np.abs(eeg_example_filt)), 'b--')
-plt.show()
+# fig = plt.figure(2)
+# plt.plot(x_tick, 20*np.log10(np.abs(eeg_example_unfilt)), 'r--', x_tick, 20*np.log10(np.abs(eeg_example_filt)), 'b--')
+# plt.show()
+
+# --- Part C - averaging time frames and plotting
+
+# average every 10 time frames
+averaged = [avg_timeframe(eeg_filtered[k:k+10]) for k in np.arange(0, t-10, 10)]
+plt.plot(x_tick, 20*np.log10(np.abs(averaged[1])))
 
 
 
